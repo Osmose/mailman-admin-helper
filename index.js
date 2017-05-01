77ade1ab7ae104195cfb11aa08dfc33722a4f520
Michael Kelly <mkelly@mozilla.com>
2017-04-30 17:37:20 -0700

    Use a DOM builder instead of innerHTML.
    Because I'm not entirely confident there's not an opportunity for an
    injection attack.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+function el(tagName, attrs={}, children=[]) {
+  const element = document.createElement(tagName);
+  for (const [key, value] of Object.entries(attrs)) {
+    element.setAttribute(key, value);
+  }
+  for (const child of children) {
+    if (child instanceof HTMLElement) {
+      element.appendChild(child);
+    } else {
+      element.appendChild(document.createTextNode(child));
+    }
+  }
+  return element;
+}
+function makeShorthand(tagName) {
+  return (...arguments) => el(tagName, ...arguments);
+}
+
+const div = makeShorthand('div');
+const li = makeShorthand('li');
+const a = makeShorthand('a');
+const input = makeShorthand('input');
+const h3 = makeShorthand('h3');
+const strong = makeShorthand('strong');
+const button = makeShorthand('button');
+const h4 = makeShorthand('h4');
+const ul = makeShorthand('ul');
+const form = makeShorthand('form');
+
 function matchParent(el, selector) {
   if (el.parentNode !== null) {
     if (el.parentNode.matches(selector)) {
@@ -12,99 +41,90 @@ function matchParent(el, selector) {
 
 const oldForm = document.querySelector('form');
 
-const recipientTables = oldForm.querySelectorAll(':scope > table > tbody > tr > td > table');
-const recipients = Array.from(recipientTables).map(table => {
-  const forwardToInput = table.querySelector('input[name^=senderforwardto]');
-  const email = decodeURIComponent(forwardToInput.name.slice('senderforwardto-'.length));
+if (oldForm) {
+  const recipientTables = oldForm.querySelectorAll(':scope > table > tbody > tr > td > table');
+  const recipients = Array.from(recipientTables).map(table => {
+    const forwardToInput = table.querySelector('input[name^=senderforwardto]');
+    const email = decodeURIComponent(forwardToInput.name.slice('senderforwardto-'.length));
 
-  const messageTableBodies = Array.from(table.querySelectorAll('strong'))
-    .filter(s => s.textContent === 'Subject:')
-    .map(s => s.parentNode.parentNode.parentNode);
-  const messages = messageTableBodies.map(tbody => {
-    return {
-      link: tbody.children[0].querySelector('a').href,
-      id: tbody.querySelector('input').value,
-      subject: tbody.children[0].querySelector('td:last-child').textContent,
-      size: tbody.children[1].querySelector('td:last-child').textContent,
-      reason: tbody.children[2].querySelector('td:last-child').textContent,
-      received: tbody.children[3].querySelector('td:last-child').textContent,
-    };
-  })
+    const messageTableBodies = Array.from(table.querySelectorAll('strong'))
+      .filter(s => s.textContent === 'Subject:')
+      .map(s => s.parentNode.parentNode.parentNode);
+    const messages = messageTableBodies.map(tbody => {
+      return {
+        link: tbody.children[0].querySelector('a').href,
+        id: tbody.querySelector('input').value,
+        subject: tbody.children[0].querySelector('td:last-child').textContent,
+        size: tbody.children[1].querySelector('td:last-child').textContent,
+        reason: tbody.children[2].querySelector('td:last-child').textContent,
+        received: tbody.children[3].querySelector('td:last-child').textContent,
+      };
+    })
 
-  return {email, messages};
-});
+    return {email, messages};
+  });
 
-const newRecipientHtml = recipients.map(recipient => {
-  const encodedEmail = encodeURIComponent(recipient.email);
+  const newForm = form({class: 'helper-form', action: oldForm.action, method: oldForm.method}, [
+    div({class: 'recipients'}, recipients.map(recipient => {
+      const encodedEmail = encodeURIComponent(recipient.email);
 
-  const messagesHtml = recipient.messages.map(message => `
-    <li>
-      <a href="${message.link}">${message.subject}</a>
-      <input type="hidden" name="${encodedEmail}" value="${message.id}">
-    </li>
-  `).join('');
+      return div({class: 'recipient', 'data-email': encodedEmail}, [
+        h3({}, [
+          strong({}, ['From:']),
+          recipient.email,
+        ]),
+        div({class: 'recipient-body'}, [
+          div({class: 'controls'}, [
+            button({class: 'accept'}, ['Accept']),
+            button({class: 'accept whitelist'}, ['Accept & Whitelist']),
+            button({class: 'discard'}, ['Discard']),
+            button({class: 'discard blacklist'}, ['Discard & Blacklist']),
+          ]),
+          div({class: 'messages'}, [
+            h4({}, ['Messages:']),
+            ul({}, recipient.messages.map(message => (
+              li({}, [
+                a({href: message.link}, [message.subject]),
+                input({type: 'hidden', name: encodedEmail, value: message.id}),
+              ])
+            ))),
+          ]),
+        ]),
+      ]);
+    })),
+  ]);
 
-  return `
-    <div class="recipient" data-email="${encodedEmail}">
-      <h3><strong>From:</strong>${recipient.email}</h3>
-      <div class="recipient-body">
-        <div class="controls">
-          <button class="accept">Accept</button>
-          <button class="accept whitelist">Accept & Whitelist</button>
-          <button class="discard">Discard</button>
-          <button class="discard blacklist">Discard & Blacklist</button>
-        </div>
-        <div class="messages">
-          <h4>Messages</h4>
-          <ul>
-            ${messagesHtml}
-          </ul>
-        </div>
-      </div>
-    </div>
-  `;
-}).join('');
-
-const newForm = document.createElement('form');
-newForm.className = 'helper-form';
-newForm.action = oldForm.action;
-newForm.method = oldForm.method;
-newForm.innerHTML = `
-  <div class="recipients">
-    ${newRecipientHtml}
-  </div>
-`;
-
-function addInput(name, value) {
-  const input = document.createElement('input');
-  input.type = 'hidden';
-  input.name = name
-  input.value = value;
-  newForm.appendChild(input);
-}
-
-newForm.addEventListener('click', event => {
-  const target = event.target;
-  if (target.matches('.controls button')) {
-    event.preventDefault();
-    const email = matchParent(target, '.recipient').dataset.email;
-    if (target.classList.contains('accept')) {
-      addInput(`senderaction-${email}`, '1');
-      if (target.classList.contains('whitelist')) {
-        addInput(`senderfilterp-${email}`, '1');
-        addInput(`senderfilter-${email}`, '6');
-      }
-      newForm.submit();
-    } else if (target.classList.contains('discard')) {
-      addInput(`senderaction-${email}`, '3');
-      if (target.classList.contains('blacklist')) {
-        addInput(`senderfilterp-${email}`, '1');
-        addInput(`senderfilter-${email}`, '3');
-      }
-      newForm.submit();
-    }
+  function addInput(name, value) {
+    newForm.appendChild(input({
+      type: 'hidden',
+      name: name,
+      value: value,
+    }));
   }
-});
 
-oldForm.parentNode.insertBefore(newForm, oldForm);
-oldForm.style.display = 'none';
+  newForm.addEventListener('click', event => {
+    const target = event.target;
+    if (target.matches('.controls button')) {
+      event.preventDefault();
+      const email = matchParent(target, '.recipient').dataset.email;
+      if (target.classList.contains('accept')) {
+        addInput(`senderaction-${email}`, '1');
+        if (target.classList.contains('whitelist')) {
+          addInput(`senderfilterp-${email}`, '1');
+          addInput(`senderfilter-${email}`, '6');
+        }
+        newForm.submit();
+      } else if (target.classList.contains('discard')) {
+        addInput(`senderaction-${email}`, '3');
+        if (target.classList.contains('blacklist')) {
+          addInput(`senderfilterp-${email}`, '1');
+          addInput(`senderfilter-${email}`, '3');
+        }
+        newForm.submit();
+      }
+    }
+  });
+
+  oldForm.parentNode.insertBefore(newForm, oldForm);
+  oldForm.style.display = 'none';
+}
